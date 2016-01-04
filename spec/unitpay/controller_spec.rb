d632bc88b9e85705dfad2dfe50e8be3cc34cdcb9
ssnikolay <ssnikolay@gmail.com>
2016-01-04 12:08:57 +0300

    refactoring test of controller

--- spec/unitpay/controller_spec.rb
+++ spec/unitpay/controller_spec.rb
@@ -1,10 +1,10 @@
 describe Unitpay::Controller do
   class TestController
     include Unitpay::Controller
-    attr_reader :request
+    attr_reader :response
 
-    def render(request)
-      @request = request
+    def render(response)
+      @response = response
     end
 
     def params
@@ -13,6 +13,18 @@ describe Unitpay::Controller do
 
     def service
       Unitpay::Service.new('public_key', 'secret_key')
+    end
+  end
+
+  RSpec::Matchers.define :be_success do |_|
+    match do |actual|
+      expect(actual).to eq(json: { result: { message: 'Запрос успешно обработан' } })
+    end
+  end
+
+  RSpec::Matchers.define :be_fail do |_|
+    match do |actual|
+      expect(actual).to eq(json: { error: { message: 'Неверная сигнатура' } })
     end
   end
 
@@ -51,58 +63,69 @@ describe Unitpay::Controller do
       }
     end
 
+    let(:sum) { 10 }
+
+    let(:response) do
+      controller = TestController.new
+      controller.notify
+      controller.response
+    end
+
     before do
       allow_any_instance_of(TestController).to receive(:params).and_return(params)
     end
+
+    subject { response }
 
     describe '#check' do
       let(:method) { 'check' }
 
       context 'when valid signature' do
-        let(:sum) { 10 }
+        before { expect_any_instance_of(TestController).to receive(:check) }
 
-        it 'should render success request' do
-          expect_any_instance_of(TestController).to receive(:check)
-          controller = TestController.new
-          controller.notify
-          expect(controller.request).to eq(json: { result: { message: 'Запрос успешно обработан' } })
-        end
+        it { is_expected.to be_success }
       end
 
       context 'when invalid signature' do
         let(:sum) { 11 }
 
-        it 'should render fail request' do
-          controller = TestController.new
-          controller.notify
-          expect(controller.request).to eq(json: { error: { message: 'Неверная сигнатура' } })
-        end
+        it { is_expected.to be_fail }
       end
     end
 
     describe '#pay' do
       let(:method) { 'pay' }
 
-      context 'when valid signature' do
-        let(:sum) { 10 }
+      it 'should raise not implemented error' do
+        controller = TestController.new
+        expect{ controller.notify }.to raise_error(Unitpay::Controller::PayNotImplementedError)
+      end
 
-        it 'should render success request' do
-          controller = TestController.new
-          expect{ controller.notify }.to raise_error(Unitpay::Controller::PayNotImplementedError)
-        end
+      context 'when valid signature' do
+        before { expect_any_instance_of(TestController).to receive(:pay) }
+
+        it { is_expected.to be_success }
+      end
+
+      context 'when invalid signature' do
+        let(:sum) { 11 }
+
+        it { is_expected.to be_fail }
       end
     end
 
     describe '#error' do
       let(:method) { 'error' }
 
-      context 'when valid signature' do
-        let(:sum) { 10 }
+      it 'should raise not implemented error' do
+        controller = TestController.new
+        expect{ controller.notify }.to raise_error(Unitpay::Controller::ErrorNotImplementedError)
+      end
 
-        it 'should render success request' do
-          controller = TestController.new
-          expect{ controller.notify }.to raise_error(Unitpay::Controller::ErrorNotImplementedError)
-        end
+      context 'when valid signature' do
+        before { expect_any_instance_of(TestController).to receive(:error) }
+
+        it { is_expected.to be_success }
       end
     end
   end
